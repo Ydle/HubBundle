6c1b815d7d5d0e21ff71e75d9898a30b0f0aa9a5
Yaug <manuel.esteban@chroniques-lunaires.com>
2014-04-27 10:50:39 +0200

    Adding menu config

--- Menu/MenuBuilder.php
+++ Menu/MenuBuilder.php
@@ -23,12 +23,36 @@ class MenuBuilder
         $menu = $this->factory->createItem('root');
         $menu->setCurrentUri($request->getRequestUri());
 
+        // home
         $dashboardTitle = $translator->trans('title.dashboard');
         $menu->addChild($dashboardTitle, array('route' => 'homeYdle'));
-        $menu->addChild('Pièces', array('route' => 'rooms'));
-        $menu->addChild('Capteurs', array('route' => 'nodes'));
-        $menu->addChild('Configuration', array('route' => 'configDashboard'));
-        $menu->addChild('A propos', array('route' => 'pagesAbout'));
+        
+        // Rooms
+        $roomsTitle = $translator->trans('title.rooms');
+        $allRoomsTitle = $translator->trans('title.rooms.all');
+        $newRoomTitle = $translator->trans('title.rooms.new');
+        $menu->addChild($roomsTitle, array('route' => 'rooms'));
+        
+        // Nodes
+        $nodesTitle = $translator->trans('title.nodes');
+        $allNodesTitle = $translator->trans('title.nodes.all');
+        $newNodeTitle = $translator->trans('title.nodes.new');
+        $menu->addChild($nodesTitle, array('route' => 'nodes'));
+        
+        // Settings
+        $configTitle = $translator->trans('title.config');
+        $configGeneralTitle = $translator->trans('title.config.general');
+        $configRoomsTitle = $translator->trans('title.config.rooms');
+        $configNodesTitle = $translator->trans('title.config.nodes');
+        $menu->addChild($configTitle, array('route' => 'configDashboard'));
+        
+        // Logs
+        $logsTitle = $translator->trans('title.logs');
+        $menu->addChild(logsTitle, array('route' => 'pagesAbout'));
+        
+        // About
+        $aboutTitle = $translator->trans('title.about');
+        $menu->addChild($aboutTitle, array('route' => 'pagesAbout'));
 
         $menu->setChildrenAttribute('class', 'sidebar-menu');
 
